5ace435755fd8b93d5ad1122f2a5652adf118977
Liaozhenting <15399901665@163.com>
2017-09-25 21:45:59 +0800

    兼容commonJS,AMD

--- cxx.js
+++ cxx.js
@@ -1,42 +1,68 @@
-(function (window) {
-    var push = Array.prototype.push;
-    var slice = Array.prototype.slice;
+(function (global, factory) {
+  "use strict";
 
-    var cxx = function (html) {
-        return new cxx.fn.init(html)
-    }
-    cxx.fn = cxx.prototype = {
-        selector: "",
-        type: 'cxx',
-        constructor: cxx,
-        length: 0,
-        init: function (html) {
-
+  if (typeof module === "object" && typeof module.exports === "object") {
+    module.exports = global.document ?
+      factory(global, true) :
+      function (w) {
+        if (!w.document) {
+          throw new Error("cxx requires a window with a document");
         }
+        return factory(w);
+      };
+  } else {
+    factory(global);
+  }
+
+  //noGlobla is boolean
+})(typeof window !== 'undefined' ? window : this, function (window, noGlobal) {
+  var push = Array.prototype.push;
+  var slice = Array.prototype.slice;
+
+  var cxx = function (html) {
+    return new cxx.fn.init(html)
+  }
+  cxx.fn = cxx.prototype = {
+    selector: "",
+    type: 'cxx',
+    constructor: cxx,
+    length: 0,
+    init: function (html) {
+
     }
+  }
 
-    cxx.extend = cxx.fn.extend = function (obj) {
-        for (var k in obj) {
-            this[k] = obj[k];
-        }
+  cxx.extend = cxx.fn.extend = function (obj) {
+    for (var k in obj) {
+      this[k] = obj[k];
     }
+  }
 
-    cxx.fn.init.prototype = cxx.fn;
+  cxx.fn.init.prototype = cxx.fn;
 
+
+
+  cxx.extend({
+    isString: function (str) {
+      return (typeof str === "string");
+    },
+    isFuncton: function (func) {
+      return (typeof func === "function");
+    },
+    isDOM: function (dom) {
+      return !!(dom.nodeType);
+    },
+    isObeject: function (obj) {
+      return (typeof obj === "object");
+    }
+  })
+  if (typeof define === "function" && define.amd) {
+    define("cxx", [], function () {
+      return cxx;
+    });
+  }
+  if (!noGlobal) {
     window.cxx = window.C = cxx;
-
-    cxx.extend({
-        isString: function (str) {
-            return (typeof str === "string");
-        },
-        isFuncton: function (func) {
-            return (typeof func === "function");
-        },
-        isDOM: function (dom) {
-            return !!(dom.nodeType);
-        },
-        isObeject: function (obj) {
-            return (typeof obj === "object");
-        }
-    })
-})(window)
+  }
+  return cxx;
+})
