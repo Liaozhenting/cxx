263c366958167868a956aa3fbc4772b1dcdd5cd0
liaozhenting <15399901665@163.com>
2017-09-26 11:30:52 +0800

    copy选择器引擎了

--- cxx.js
+++ cxx.js
@@ -196,6 +196,231 @@
   })
 
   //dom操作
+  cxx.fn.extend({
+    appendTo: function (dom) {
+      var targetObj = cxx(dom);
+      var newObj = cxx();
+      for (var i = 0; i < this.length; i++) {
+        for (var j = 0; j < targetObj.length; j++) {
+          var temp = (j == targetObj.length - 1 ? this[i] : this[i].cloneNode(true));
+
+          target[j].appendChild(temp);
+          push.call(newObj, temp);
+        }
+      }
+      return newObj;
+    },
+    append: function (dom) {
+      cxx(dom).appendTo(this);
+      return this;
+    },
+    prependTo: function (dom) {
+      var targetObj = cxx(dom);
+      var newObj = cxx();
+      for (var i = 0; i < this.length; i++) {
+        for (var j = 0; j < target.length; j++) {
+          var temp = (j === target.length - 1 ? this[i] : this[i].cloneNode(true));
+          var firstChild = targetObj[j].firstChild;
+          targetObj[j].insertBefore(temp, firstChild);
+          push.call(newObj, temp)
+        }
+      }
+      return newObj;
+    },
+    prepend: function (dom) {
+      cxx(dom).prependTo(this);
+      return this;
+    }
+  })
+
+  //选择器引擎
+  var select = (function () {
+    var rnative = /\{\s*\[native/;
+    var rBaseSeletor = /^(?:\#([\w\-]+)|\.([\w\-]+)|(\*)|(\w+))$/;
+    var rtrim = /^\s+|\s+$/g
+    var support = {};
+    //方法定义检测
+    support.qsa = rnative.test(document.querySelectorAll + '');
+    support.getElementsByClassName = rnative.test(document.getElementsByClassName + '');
+    support.trim = rnative.test("".trim + '');
+    support.indexOf = rnative.test(Array.prototype.indexOf + '');
+    //support.qsa = false;
+
+
+
+
+    //兼容的getElementsByClassName方法
+    function getByClassName(className, node) {
+      node = node || document;
+      var allElem, res = [], i;
+
+      if (support.getElementsByClassName) {
+        var result = document.getElementsByClassName(className);
+        push.apply(res, result);
+        return res;
+      }
+      else {
+        allElem = document.getElementsByTagName("*");
+        for (i = 0; i < allElem.length; i++) {
+          if ((" " + allElem[i].className + " ").indexOf(" " + className + " ") >= 0) {
+            push(allElem[i]);
+          }
+        }
+        return res;
+      }
+    }
+
+
+
+    //函数myTrim，去除掉str前后的空格，并返回str
+    //str :  "  hello   "  =>  myTrim(str)   =>  "hello"
+    function myTrim(str) {
+      //如果系统支持trim方法，就使用trim方法去除掉字符串前后的空格
+      if (support.trim) {
+        return str.trim();
+      }
+      //如果系统不支持trim方法，那就使用正则将字符串前后的空格替换为''
+      else {
+        return str.replace(rtrim, '');
+      }
+    }
+    //实现兼容的indexOf方法，因为ie6,7,8不支持数组的indexOf方法
+    //返回对应元素的下标  
+    //array : [1,2,3,4]  ,search: 3
+    //myIndexOf(array,search)   =>  在array数组中查找search元素，并返回search所在的下标
+    //返回的下标就是2.  如果没有找到元素就会返回-1
+    function myIndexOf(array, search, startIndex) {
+      startIndex = startIndex || 0;
+      //如果系统支持indexOf方法
+      if (support.indexOf) {
+        return array.indexOf(search, startIndex);
+      }
+      else {	//自己实现indexOf方法
+        for (var i = startIndex; i < array.length; i++) {
+          //如果循环的第i个元素和要查找的search元素一致
+          if (array[i] === search) {	//返回该元素的下标
+            return i;
+          }
+        }
+        //如果循环完毕没有找到一致的元素，就返回-1表示没有找到一致的元素
+        return -1;
+      }
+    }
+    //元素去重函数：去除掉参数array数组中的重复元素
+    //array : [1,2,3,3,4,5,1]  =>  unique(array)  =>  array:[1,2,3,4,5];
+    function unique(array) {	//定义一个空数组
+      var resArray = [];
+      //循环array中的所有元素
+      for (var i = 0; i < array.length; i++) {	//判断resArray数组中是否包含了array[i]元素，如果没有包含
+        if (myIndexOf(resArray, array[i]) == -1) {	//就把这个元素添加到resArray数组中来。
+          resArray.push(array[i]);
+        }
+      }
+      //返回去重后的数组
+      return resArray;
+    }
+
+
+    //选择器函数select，通过该函数可以选择页面中的某些元素
+    //参数解释：selector 传递选择器字符串  。  results  传入一个数组，将查询到的元素加入该数组
+    //如何调用select函数：  
+    /*
+        select("#dv");  select(".cc");  select("*");  select("div");
+        select("#dv,  .cc ,  div");
+    */
+
+    //在node节点中查找选择器字符串为selector的内容
+    //document 找 selector是#dv的元素
+    function basicSelect(selector, node) {
+      var results = [];
+
+      var m = rBaseSeletor.exec(selector);
+      if (m) {
+        if (m[1]) {
+          var temp = document.getElementById(m[1]);
+          if (temp) {
+            results.push(temp);
+          }
+        }
+        else if (m[2]) {
+          push.apply(results, getByClassName(m[2], node));
+        }
+        else if (m[3] || m[4]) {
+          push.apply(results, node.getElementsByTagName(selector));
+        }
+      }
+
+      //返回包含了元素的数组
+      return unique(results);
+    }
+    //获得后代选择器选择的元素
+    function select2(selector, results) {
+      results = results || [];
+      //进来的selector是'div      div'=>把多个空格换成一个空格
+      selectors.replace(/\s+/g, " ");
+      //假定selectors => ['div','div']
+      var selectors = selector.split(' ');
+
+      //arr存的是所有的div
+      var arr = [], node = [document];
+      //循环空格分隔出来的选择器字符串
+      for (var j = 0; j < selectors.length; j++) {
+        for (var i = 0; i < node.length; i++) {
+          //basicSelect(selectors[0], node[i] )：在document中寻找所有的div元素
+          //把这些元素添加到arr数组中
+          push.apply(arr, basicSelect(selectors[j], node[i]));
+        }
+        node = arr; //arr里有所有的div，  node里也有所有的div
+        arr = []; //arr里面什么都没有了，  
+      }
+      push.apply(results, node);
+
+      return results;
+    }
+    //select函数是选择器引擎，入口函数,用户调用select函数来获得对应的元素
+    //select('div');  select("#dv");  select(".cc"); select("*");
+    //select("div,#dv,.cc"); 组合选择器
+    //select('div div,div p,div p .c');
+    function select(selector, results) {
+      results = results || [];
+      if (typeof selector != 'string') {
+        return results;
+      }
+      //如果系统支持qsa，就使用qsa获得元素
+      if (support.qsa) {
+        push.apply(results, document.querySelectorAll(selector));
+        return results;
+      }
+      else//否则自己实现
+      {
+        //按照逗号分隔选择器字符串 select('div div,div p,div p .c','span');
+        //=>['div div','div p','div p .c','span']
+        var selectors = selector.split(",");
+        //循环所有的选择器字符串
+        for (var i = 0; i < selectors.length; i++) {	//对循环的那一个字符串去除前后空格
+          var subSelector = myTrim(selectors[i]);
+          //用正则表达式匹配选择器字符串，
+          //如果匹配成功代表肯定是id选择器，类选择器，*，标签名其中一种
+          if (rBaseSeletor.test(subSelector)) {
+            push.apply(results, basicSelect(subSelector, document));
+          }
+          //如果匹配不成功，表示不是基本选择器的四种形式，应该就是后代选择器
+          else {	//使用select2函数获得后代选择器的元素
+            results = select2(subSelector, results);
+          }
+        }
+      }
+      //返回去重复了的元素数组
+      return unique(results);
+    }
+
+    //返回select函数作为沙箱的结果
+    return select;
+
+
+  })();
+
+  cxx.select = select;
 
 
 
